dab24c7104b7f3a696b8ebd55e6e1ff455f2ec43
ClaudiaPrez88 <58665197+ClaudiaPrez88@users.noreply.github.com>
2025-08-04 16:47:24 -0400

    Agrega implementación de redux toolkit para doctores con fetch a API pública

--- src/app/(admin)/(ui-elements)/videos/page.tsx
+++ src/app/(admin)/(ui-elements)/videos/page.tsx
@@ -1,20 +1,19 @@
-import PageBreadcrumb from "@/components/common/PageBreadCrumb";
-import VideosExample from "@/components/ui/video/VideosExample";
-import { Metadata } from "next";
-import React from "react";
+// src/app/(admin)/(ui-elements)/videos/page.tsx  (componente server)
+import PageBreadcrumb from "@/components/common/PageBreadCrumb"
+import VideosExample from "@/components/ui/video/VideosExample"
+import ListaDoctoresClient from "@/components/user-profile/ListaDoctoresClient"
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Next.js Videos | TailAdmin - Next.js Dashboard Template",
-  description:
-    "This is Next.js Videos page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
-};
+  description: "This is Next.js Videos page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+}
 
 export default function VideoPage() {
   return (
     <div>
       <PageBreadcrumb pageTitle="Videos" />
-
       <VideosExample />
+      <ListaDoctoresClient />
     </div>
-  );
+  )
 }
