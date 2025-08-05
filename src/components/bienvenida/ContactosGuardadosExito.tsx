04b54e8ed406cb2414aadf30311f4481b62fe116
ClaudiaPrez88 <58665197+ClaudiaPrez88@users.noreply.github.com>
2025-08-04 20:13:00 -0400

    Cambiar colores

--- src/components/bienvenida/ContactosGuardadosExito.tsx
+++ src/components/bienvenida/ContactosGuardadosExito.tsx
@@ -1,14 +1,22 @@
 "use client";
 import React from "react";
 import Button from "../ui/button/Button";
+import { useRouter } from "next/navigation";
+
+
 
 const ContactosGuardadosExito = () => {
+  const router = useRouter();
+
+  const handleIniciarClick = () => {
+    router.push("/");
+  };
   return (
-    <div className="max-w-md p-6 bg-green-100 rounded">
+    <div className="max-w-md p-6 bg-green-100 rounded-2xl">
       <h2 className="text-xl font-bold mb-4">¡Contactos guardados correctamente!</h2>
       <p>Ahora puedes comenzar a usar la aplicación.</p>
       <div className="py-4">
-        <Button>Iniciar</Button>
+        <Button onClick={handleIniciarClick}>Iniciar</Button>
       </div>
     </div>
   );
